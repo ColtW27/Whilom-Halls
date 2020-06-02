5d7c64dd54fc5243e9fa15a0d452b335927a9727
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-06-02 10:22:49 -0400

    Added store and root to entry file

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,9 +1,9 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-// import Root from './components/root'
+import Root from './components/root'
 import configureStore from './store/store';
-import { login, logout } from './actions/session_actions'
+// import { login, logout } from './actions/session_actions'
 //  import { login } from './util/session_api_util' // for testing api util login
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
@@ -15,5 +15,5 @@ document.addEventListener("DOMContentLoaded", () => {
     // window.logout = logout;
   //Testing End
 
-    ReactDOM.render(<h1>This is Whilom Halls</h1>, root);
+    ReactDOM.render(< Root store={store}/>, root);
 });
