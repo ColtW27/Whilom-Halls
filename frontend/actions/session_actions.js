ef18b85a33253d750e56e994a3df5cf5c91e8530
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-06-02 09:57:57 -0400

    Added session actions and sessions reducer

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,38 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+export const receiveCurrentUser = currentUser => {
+    return {
+        type: RECEIVE_CURRENT_USER,
+        currentUser,
+    };
+};
+
+export const logoutCurrentUser = () => {
+    return {
+        type: LOGOUT_CURRENT_USER,
+    };
+};
+
+export const receiveErrors = errors => {
+    return {
+        type: RECEIVE_SESSION_ERRORS,
+        errors
+    }
+}
+
+export const signup = (user) => (dispatch) => SessionAPIUtil.signup(user)
+    .then(Currentuser => dispatch(receiveCurrentUser(Currentuser)));
+
+export const login = (user) => {
+    return (dispatch) => SessionAPIUtil.login(user)
+        .then(currentUser => dispatch(receiveCurrentUser(currentUser)));
+}
+export const logout = () => {
+    debugger
+    return (dispatch) => SessionAPIUtil.logout()
+        .then(() => dispatch(logoutCurrentUser()));
+}
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,8 +1,19 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-
+// import Root from './components/root'
+// import configurStore from './store/store';
+// import { login, logout } from './actions/session_actions'
+ import { login } from './util/session_api_util' // for testing api util login
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
+
+  //Testing Start
+    // window.getState = store.getState;
+    // window.dispatch = store.dispatch;
+    window.login = login;
+    // window.logout = logout;
+  //Testing End
+
     ReactDOM.render(<h1>This is Whilom Halls</h1>, root);
 });
