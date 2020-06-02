8c389318721acf52577ad65bbfa12e0c93cfbfc6
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-06-02 10:19:23 -0400

    Added in App.jsx

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,15 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import Root from './components/root'
-// import configurStore from './store/store';
-// import { login, logout } from './actions/session_actions'
- import { login } from './util/session_api_util' // for testing api util login
+import configureStore from './store/store';
+import { login, logout } from './actions/session_actions'
+//  import { login } from './util/session_api_util' // for testing api util login
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-
+    const store = configureStore();
   //Testing Start
-    // window.getState = store.getState;
-    // window.dispatch = store.dispatch;
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     window.login = login;
     // window.logout = logout;
   //Testing End
