c9529df2e7d44cf2ce45cbd4231dfabc505e35df
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-06-07 21:43:17 -0400

    Started seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "lillyflower101@user.com", :password: "123456", first_name: "Lilly", last_name: "Flower")
+
+User.create(email: "linziemirth31@user.com", :password: "123456", first_name: "Linzie", last_name: "Mirth")
+
+User.create(email: "Allisterprim86@user.com", :password: "123456", first_name: "Allister", last_name: "Prim")
+
+User.create(email: "Forestlarks9@user.com", :password: "123456", first_name: "Forest", last_name: "Larks")
+
+User.create(email: "edgarmoralez22@user.com", :password: "123456", first_name: "Edgar", last_name: "Moralez")
+
+Listing.create(name: "Allerdale Hall", latitude: 51.225039 , longitude: -3.689588, size: 12666, bedrooms: 16, bathrooms: 8, pricing: 380, description: "Allerdale Hall is a terrifyingly gorgeous gothic mansion on the moors of England",max_num_guests: 30 )
+
+Listing.create(name: "Wuthering Heights", latitude: 51.641389 , longitude: -4.704266, size: 6735, bedrooms: 9, bathrooms: 5, pricing: 355, description: "Wuthering Heights, the windy and looming home of Mr. Heathecliff.",max_num_guests: 19 )
+
+Listing.create(name: "Maiden Spire", latitude: 50.137318 , longitude: -5.377226, size: 9088, bedrooms: 10, bathrooms: 6, pricing: 703, description: "Maiden Spire, known for its single tower that strethes toward the sky, sits in the southwest tip of England.",max_num_guests: 18 )
+
+Listing.create(name: "Draves Manor", latitude: 52.201135 , longitude: -7.455342, size: 10656, bedrooms: 12, bathrooms: 6, pricing: 478, description: "Stately manorhouse in Ireland.",max_num_guests: 17 )
+
+Listing.create(name: "Bride's Head Castle", latitude: 43.068014 , longitude: -70.715053, size: 8845, bedrooms: 9, bathrooms: 4, pricing: 280, description: "",max_num_guests: 20 )
+
+Listing.create(name: "Manderley Mansion", latitude: 51.225039 , longitude: -3.689588, size: 12666, bedrooms: 16, bathrooms: 8, pricing: 380, description: "",max_num_guests: 30 )
+
+Listing.create(name: "Xandau", latitude: 51.225039 , longitude: -3.689588, size: 12666, bedrooms: 16, bathrooms: 8, pricing: 380, description: "",max_num_guests: 30 )
+
+Listing.create(name: "Thrushcross Grange", latitude: 51.225039 , longitude: -3.689588, size: 12666, bedrooms: 16, bathrooms: 8, pricing: 380, description: "",max_num_guests: 30 )
+
+Listing.create(name: "Hill House", latitude: 51.225039 , longitude: -3.689588, size: 12666, bedrooms: 16, bathrooms: 8, pricing: 380, description: "",max_num_guests: 30 )
+
+Listing.create(name: "Thornfield Hall", latitude: 51.225039 , longitude: -3.689588, size: 12666, bedrooms: 16, bathrooms: 8, pricing: 380, description: "",max_num_guests: 30 )
+
+Listing.create(name: "Gardencourt", latitude: 51.225039 , longitude: -3.689588, size: 12666, bedrooms: 16, bathrooms: 8, pricing: 380, description: "",max_num_guests: 30 )
+
+Listing.create(name: "The Castellated Abbey", latitude: 51.225039 , longitude: -3.689588, size: 12666, bedrooms: 16, bathrooms: 8, pricing: 380, description: "",max_num_guests: 30 )
+
+Listing.create(name: "Barton Cottage", latitude: 51.225039 , longitude: -3.689588, size: 12666, bedrooms: 16, bathrooms: 8, pricing: 380, description: "",max_num_guests: 30 )
+
+Listing.create(name: "Satishouse", latitude: 51.225039 , longitude: -3.689588, size: 12666, bedrooms: 16, bathrooms: 8, pricing: 380, description: "With the clocks all stopped at 20 'til nine.",max_num_guests: 30 )
+
+Listing.create(name: "Wildfell Hall", latitude: 51.225039 , longitude: -3.689588, size: 12666, bedrooms: 16, bathrooms: 8, pricing: 380, description: "",max_num_guests: 30 )
+
+Listing.create(name: "House of Usher", latitude: 51.225039 , longitude: -3.689588, size: 12666, bedrooms: 16, bathrooms: 8, pricing: 380, description: "",max_num_guests: 30 )
+
+Listing.create(name: "Castle Udolphe", latitude:  , longitude: , size: , bedrooms: , bathrooms: , pricing: , description: "", max_num_guests: )
+
+Listing.create(name: "Seven Gables", latitude:  , longitude: , size: 7777, bedrooms: 7, bathrooms: 7, pricing: 777, description: "We wish you the best of luck with your stay here at Seven Gables.", max_num_guests: 17 )
+
+Listing.create(name: "Pemberly Brook", latitude:  , longitude: , size: , bedrooms: , bathrooms: , pricing: , description: "Large, cold, and mostly..well, mostly all stone. At least the fireplaces seem to light themselves.", max_num_guests: )
+
+Listing.create(name: "Ravenhurst", latitude:  , longitude: , size: , bedrooms: , bathrooms: , pricing: , description: "", max_num_guests: )
+
+Listing.create(name: "Skulagato Row", latitude:  , longitude: , size: , bedrooms: , bathrooms: , pricing: , description: "", max_num_guests: )
+
+Listing.create(name: "Misselthwaite Manor", latitude:  , longitude: , size: , bedrooms: , bathrooms: , pricing: , description: "", max_num_guests: )
+
+# Listing.create(name: "", latitude:  , longitude: , size: , bedrooms: , bathrooms: , pricing: , description: "", max_num_guests: )
+
