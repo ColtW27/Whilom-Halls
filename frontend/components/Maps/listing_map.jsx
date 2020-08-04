a800f3e3520756274c0a692d0caa93637fd8c611
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-08-04 18:45:02 -0400

    Render google map without key

--- frontend/components/Maps/listing_map.jsx
+++ frontend/components/Maps/listing_map.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+
 
 class ListingMap extends React.Component {
+
+    componentDidMount() {
+        const mapOptions = {
+            center: { lat: 44.225507, lng: -68.324317 },
+            zoom: 13
+        };
+      
+        this.map = new google.maps.Map(this.mapNode, mapOptions);
+    }
   render(){
-      <div id="map-container" ref="map"></div>
+      return (
+        <div id="map-container" ref={(map) => (this.mapNode = map)}></div>
+      );
   }
 }
 
-export default ListingMap;
+// export default ListingMap;
+export default GoogleApiWrapper({
+//   apiKey: MAPS_API_KEY
+  //   apiKey: `${MAPS_API_KEY}`/
+    
+})(ListingMap);
