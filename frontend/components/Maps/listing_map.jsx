bb8a719a9683a26906d1c74f987630fa146f343c
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-08-05 20:09:34 -0400

    Debug env variable issue

--- frontend/components/Maps/listing_map.jsx
+++ frontend/components/Maps/listing_map.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 const MAPS_API_KEY = ENV["MAPS_API_KEY"];
-function statusChecker(input) {
+// const MAPS_API_KEY = MAPS_API_KEY;
 
+function statusChecker(input) {
   return (
     <Marker
       title={input.name}
@@ -23,6 +24,7 @@ class ListingMap extends React.Component {
     //     this.map = new google.maps.Map(this.mapNode, mapOptions);
     // }
   render(){
+     
       const {listing, listings} = this.props;
       return (
         <Map
@@ -58,9 +60,9 @@ class ListingMap extends React.Component {
 
 // export default ListingMap;
 export default GoogleApiWrapper({
-    apiKey: MAPS_API_KEY
+    // apiKey: MAPS_API_KEY
     // apiKey: `${ENV["API_KEY"]}`
     // apiKey: `${MAPS_API_KEY}`
     // apiKey: `${ENV['MAPS_API_KEY']}`
-//   
+
 })(ListingMap);
