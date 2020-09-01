5e3af3b33d3d933f7c27ca19907653220efafc0f
Colton Wilmouth <cwilmouth27@yahoo.com>
2020-09-01 19:05:10 -0400

    Change future destinations to actual places with seeded data

--- frontend/components/future_dests_bar/future_destinations.jsx
+++ frontend/components/future_dests_bar/future_destinations.jsx
@@ -11,27 +11,34 @@ const FutureDestinations = () => {
        
           
         <ul className="individual-dest left" >
-            <li><Link to="/">Lawrence, Kansas</Link></li>
-            {/* <li><Link to="/">Belmont, North Carolina</Link></li>
+          <li><Link to="/">Rockport, Massachusetts</Link></li>
+            {/* Ravenhurst */}
+            {/* <li><Link to="/">Lawrence, Kansas</Link></li>
+            <li><Link to="/">Belmont, North Carolina</Link></li>
             <li><Link to="/">Indianapolis, Indiana</Link></li> */}
         </ul>
 
         <ul className="individual-dest" >
           <li><Link to="/">Porlock Minehead, England</Link></li>
+          {/* Allerdale Hall */}
             {/* <li><Link to="/">Knoxville, Tennessee</Link></li>
             <li><Link to="/">Galena, Illinois</Link></li>
             <li><Link to="/">Eureka, California</Link></li> */}
         </ul>
 
         <ul className="individual-dest" >
-            <li><Link to="/">Providence, Rhode Island</Link></li>
-            {/* <li><Link to="/">Hartford, Connecticut</Link></li>
+          <li><Link to="/">Southwest Harbor, Maine</Link></li>
+          {/* Thornfield hall */}
+            {/* <li><Link to="/">Providence, Rhode Island</Link></li>
+            <li><Link to="/">Hartford, Connecticut</Link></li>
             <li><Link to="/">Lynchburg, Virginia</Link></li> */}
         </ul>
 
         <ul className="individual-dest" >
-            <li><Link to="/">Georgetown, Texas</Link></li>
-            {/* <li><Link to="/">Cheyenne, Wyoming</Link></li>
+          <li><Link to="/">Llancadle Barry Wales, United Kingdom</Link></li>
+          {/* Clairemore */}
+            {/* <li><Link to="/">Georgetown, Texas</Link></li>
+            <li><Link to="/">Cheyenne, Wyoming</Link></li>
             <li><Link to="/">Portsmouth, New Hampshire</Link></li> */}
         </ul>
       </div>
